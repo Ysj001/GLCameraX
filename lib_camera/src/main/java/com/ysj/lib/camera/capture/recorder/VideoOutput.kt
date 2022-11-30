09bafa5a3b5ea1f83f9fc577a6b671a34a7f18f7
Ysj <543501451@qq.com>
2022-11-30 23:53:13 +0800

    feat：定义视频输出接口

---
+++ lib_camera/src/main/java/com/ysj/lib/camera/capture/recorder/VideoOutput.kt
@@ -0,0 +1,65 @@
+package com.ysj.lib.camera.capture.recorder
+
+import android.util.Size
+import android.view.Surface
+import java.util.concurrent.Executor
+
+/**
+ * 视频输出接口。
+ *
+ * @author Ysj
+ * Create time: 2022/11/3
+ */
+interface VideoOutput {
+
+    /**
+     * 当该 output 被安装时回调。
+     * - 该回调也执行在 [onAttach] 提供的 [Executor] 环境中。
+     *
+     * @param executor 用于执行内部调用的执行器，该执行器必须保证顺序执行。
+     */
+    fun onAttach(executor: Executor)
+
+    /**
+     * 当该 output 被卸载时回调。
+     * - 该回调也执行在 [onAttach] 提供的 [Executor] 环境中。
+     */
+    fun onDetach()
+
+    /**
+     * 当视频帧生产者准备好向 output 提供视频帧时回调。
+     * - 当该 output 准备好 [Surface] 并要开始编码视频帧时，即会回调 [SurfaceRequest.onRequest]
+     * 来通知生产者向该 [Surface] 提供数据。
+     * - 当该 output 编码完成视频时，即会回调 [SurfaceRequest.onRelease]
+     * 来通知生产者不需要在继续提供视频帧了。
+     * - 该回调也执行在 [onAttach] 提供的 [Executor] 环境中。
+     */
+    fun onSurfaceRequest(request: SurfaceRequest)
+
+    interface SurfaceRequest {
+        /**
+         * 当前的相机 id。
+         */
+        val cameraId: String
+
+        /**
+         * 输出视频的大小。
+         */
+        val size: Size
+
+        /**
+         * 输出视频是携带的旋转信息。
+         */
+        val degree: Int
+
+        /**
+         * 当 [Surface] 准备好时回调。
+         */
+        val onRequest: (Surface) -> Unit
+
+        /**
+         * 当 [Surface] 即将释放时回调。
+         */
+        val onRelease: (Surface) -> Unit
+    }
+}
